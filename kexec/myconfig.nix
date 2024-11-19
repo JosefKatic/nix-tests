b6ac971b85abb02ca0cd2fbc8ee44ab3f1fe5395
Josef Katič <josef@joka00.dev>
2024-11-19 17:26:52 +0100

    Fix git

--- kexec/myconfig.nix
+++ kexec/myconfig.nix
@@ -9,8 +9,7 @@
   networking.useDHCP = true;
 
   kexec.autoReboot = false;
-  environment.systemPackages = [pkgs.git pkgs.nano];
-
+  
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOyTynZ2jFg3/FderkubGqccjY1lI+h4vLCqT7NQMNp openpgp:0x4AF78C5A"
   ];
