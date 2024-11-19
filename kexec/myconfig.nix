d8f544f2153ddf931904d0f7fa2ff733f5e56d26
Josef Katič <josef@joka00.dev>
2024-11-19 17:25:01 +0100

    Add git

--- kexec/myconfig.nix
+++ kexec/myconfig.nix
@@ -4,11 +4,12 @@
   ];
 
   # Make it use predictable interface names starting with eth0
-  boot.kernelParams = [ "net.ifnames=0" ];
+  boot.kernelParams = ["net.ifnames=0"];
 
   networking.useDHCP = true;
 
   kexec.autoReboot = false;
+  environment.systemPackages = [pkgs.git pkgs.nano];
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOyTynZ2jFg3/FderkubGqccjY1lI+h4vLCqT7NQMNp openpgp:0x4AF78C5A"
